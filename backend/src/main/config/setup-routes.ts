5c81a724cccce27446e434cabcceafa91b941f44
joaonotfound <joaonotfound@protonmail.com>
2022-10-19 16:59:31 -0300

    temp: committing current state of the project

--- backend/src/main/config/setup-routes.ts
+++ backend/src/main/config/setup-routes.ts
@@ -5,6 +5,9 @@ import * as express from 'express'
 import { Controller } from "@/presentation/models/controller-model"
 import { registerRoute } from "./register-route"
 import { PrivateController } from "@/presentation/models/private-controller"
+import { OrmRepository } from "@/infra/mikro-orm/protocols/orm-repository"
+import { mikroHelper } from "@/infra/mikro-orm/helpers/mikro-helper"
+// import { RouteModel } from "@/presentation/models/route-model"
 
 function classExtends(child: any, parent: any){
     return Object.getOwnPropertyDescriptor(child, 'prototype')?.value instanceof parent
@@ -13,17 +16,27 @@ function classExtends(child: any, parent: any){
 export function setupRoutes(app: express.Application){
     const controllers = loadControllers()
     
-    controllers.forEach((controller: any) => {
+    controllers.forEach(async (controller: any) => {
         let router: Router = Router()
-        const instance: Controller = new controller()
+        const repository = new OrmRepository(await mikroHelper.getEm())
+        const instance: Controller = new controller(repository)
         const routes = instance.export()
+
         if(classExtends(controller, PrivateController)){
             router.use((req: any, res: express.Response, next: any) => {
                 res.send({ message: "essa rota é privada"})
                 req; next;
             })
         }
-        routes.forEach(route => registerRoute(router, { base_url: instance.base_url, route }))
+        
+        routes.forEach(route =>{ 
+            const bounded_route = {
+                ...route,
+                callback: Object.bind(instance, route.callback)(),
+            }
+            // console.log(bounded_route)
+            registerRoute(router, { base_url: instance.base_url, route: bounded_route })
+        })
     
         app.use(router)
     })
--- backend/src/main/index.ts
+++ backend/src/main/index.ts
@@ -1,8 +1,12 @@
 import { setupRoutes } from "./config/setup-routes";
 import { makeApp } from "./factory/app";
 
-const app = makeApp()
-setupRoutes(app)
-app.listen(5000, () => {
-  console.log('listening on localhost:5000')
-})
+async function main(){
+  const app = makeApp()
+  await setupRoutes(app)
+  app.listen(5000, () => {
+    console.log('listening on localhost:5000')
+  })
+}
+
+main()
