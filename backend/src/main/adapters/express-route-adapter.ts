da1b0f39a0b7fe5b9b9f33f4c695fab78795083c
joaonotfound <joaonotfound@protonmail.com>
2022-10-21 16:06:43 -0300

    feat: handle unexpected exceptions on controllers

--- backend/src/main/adapters/express-route-adapter.ts
+++ backend/src/main/adapters/express-route-adapter.ts
@@ -5,8 +5,14 @@ import { HttpRequestAdapter } from "./http-request-adapter";
 
 export function ExpressRouteAdapter(route: (req: HttpRequest) => Promise<HttpResponse>) {
     return async (request: Request, response: Response) => {
-        const route_response = await route(HttpRequestAdapter(request))
+        try{ 
+            const route_response = await route(HttpRequestAdapter(request))
+            response.status(route_response.status_code).send(route_response.body)            
+        }catch(error){
+            response.status(500).send({ message: 'INTERNAL SERVER ERROR.'})
+            console.log('An error ocurred: ', error)
+            console.log('request: ', HttpRequestAdapter(request))
+        }
 
-        response.status(route_response.status_code).send(route_response.body)
     }
 }
