ae5c1ce4640b474ba754814bb91f5fbca8d958be
joaonotfound <joaonotfound@protonmail.com>
2022-10-22 20:31:31 -0300

    refactor: change error message

--- backend/src/presentation/helpers/missing-param.ts
+++ backend/src/presentation/helpers/missing-param.ts
@@ -2,5 +2,5 @@ import { HttpResponse } from "../protocols";
 import { badRequest } from "./bad-request";
 
 export function missingParam(param: string): HttpResponse {
-    return badRequest({ error: `missing param: ${param}`})
+    return badRequest({ error: `missing parameter ${param}`})
 }
