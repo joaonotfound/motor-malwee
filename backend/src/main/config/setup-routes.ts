18dc138e6d259c4cda3b72a420dfd78b1ccb50a0
joaonotfound <joaonotfound@protonmail.com>
2022-10-22 14:25:15 -0300

    refactor: clean code

--- backend/src/main/config/setup-routes.ts
+++ backend/src/main/config/setup-routes.ts
@@ -8,34 +8,30 @@ import { HttpRequest, HttpResponse } from '@/presentation/protocols'
 import { MikroRepository } from '@/infra/mikro-orm/mikro-repository'
 import { entities } from '../entities'
 
-function classExtends(child: any, parent: any){
-    return Object.getOwnPropertyDescriptor(child, 'prototype')?.value instanceof parent
+function createRouter(controller: any){
+    const router = Router()        
+    const isPrivate = controller instanceof PrivateController
+    console.log('registrando rota', controller.method, controller.base_url, 'is private:', isPrivate)
+    if(isPrivate){
+        router.use((req: any, res: express.Response, next: any) => {
+            res.send({ message: "essa rota é privada" })
+            req; next;
+        })
+    }
+    const callback =  (req: HttpRequest ): Promise<HttpResponse> => controller.handle(req) 
+    registerRoute(router, controller.base_url, { method: controller.method, callback } )
+    return router
 }
 
 export async function setupRoutes(app: express.Application){
     const controllers = loadControllers()    
     const repository = await MikroRepository.create(entities)
 
-    controllers.forEach(async (controller: any) => {
-
-        const router = Router()        
+    controllers.forEach(async (controller: any) => {        
         const instance = new controller(repository)
-        const isPrivate = classExtends(controller, PrivateController)
-    
-        if(isPrivate){
-            router.use((req: any, res: express.Response, next: any) => {
-                res.send({ message: "essa rota é privada" })
-                req; next;
-            })
-        }
-
-        console.log('registrando rota', instance.method, instance.base_url, 'is private:', isPrivate)
-
-        const callback =  (req: HttpRequest ): Promise<HttpResponse> => instance.handle(req) 
-        registerRoute(router, instance.base_url, { method: instance.method, callback } )
-    
+        const router = createRouter(instance)        
         app.use(router)
-    })
+})
 }
 
 
