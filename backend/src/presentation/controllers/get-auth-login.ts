af3c1066f2c791d67b7c8b459f13648459fb5253
joaonotfound <joaonotfound@protonmail.com>
2022-10-22 13:24:39 -0300

    fix: imports

--- backend/src/presentation/controllers/get-auth-login.ts
+++ backend/src/presentation/controllers/get-auth-login.ts
@@ -1,9 +1,10 @@
-import { userEntity } from "@/data/entities/user-entity";
+import { userEntity } from "@/domain/entities/user-entity";
+import { Put } from "../decorators";
 import { ok } from "../helpers/ok";
 import { PublicController } from "../models/public-controller";
 
+@Put('/auth/login')
 export default class Login extends PublicController {
-    base_url: string = '/auth/login'
     async handle(){
         console.log(await this.repository.collection(userEntity).find({ id: 1 }))
         return ok({ message: 'rota alcançada com sucesso. '})
