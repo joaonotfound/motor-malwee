647e4b4631ad99793f05ebcceefd4d15d4f17cde
joaonotfound <joaonotfound@protonmail.com>
2022-10-22 16:39:39 -0300

    chore: rename reject helper

--- backend/src/presentation/helpers/bad-request.ts
+++ backend/src/presentation/helpers/bad-request.ts
@@ -1,6 +1,6 @@
 import { HttpResponse } from "../protocols";
 
-export function reject<T=any>(data: T): HttpResponse<T> {
+export function badRequest<T=any>(data: T): HttpResponse<T> {
     return {
         status_code: 400,
         body: data
