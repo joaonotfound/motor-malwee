5b09251cf95ff8a3768aa1e4fed89a4e6193e1a3
joaonotfound <joaonotfound@protonmail.com>
2022-10-22 21:25:47 -0300

    test: should return 400 if invalid credentials

--- backend/src/presentation/controllers/authentication/login.spec.ts
+++ backend/src/presentation/controllers/authentication/login.spec.ts
@@ -1,8 +1,16 @@
-import { missingParam, invalidParam } from "@/presentation/helpers"
+import { missingParam, invalidParam, invalidCredentials } from "@/presentation/helpers"
 import { HttpRequest } from "@/presentation/protocols"
 import { LoginController } from "./login"
-import { EmailValidator } from "@/domain"
+import { EmailValidator, Encrypter } from "@/domain"
+import { createRepositoryStub } from '../../helpers/create-repository-stub'
 
+const makeEncrpterStub = () => {
+    return new class EncrypterStub implements Encrypter {
+        async encrypt(_: string): Promise<string> {
+            return 'hashed_password'
+        }        
+    }
+}
 const makeEmailValidatorStub = () => {
     class EmailValidatorStub implements EmailValidator {  
         async validate(_: string) {
@@ -14,12 +22,14 @@ const makeEmailValidatorStub = () => {
 
 const makeSut = () => {
     const emailValidator = makeEmailValidatorStub()
-    const sut = new LoginController(emailValidator)
-    return { sut, emailValidator }
+    const encrypterStub = makeEncrpterStub()
+    const { repositoryStub, collectionStub } = createRepositoryStub()
+    const sut = new LoginController(emailValidator, repositoryStub, encrypterStub)
+    return { sut, emailValidator, encrypterStub, repositoryStub, collectionStub }
 }
 
 describe('Login Controller', () => {
-    test('should 400 if no username is provided', async () => {
+    test('should return 400 if no username is provided', async () => {
         const { sut } = makeSut()
         const request: HttpRequest = {
             params: {
@@ -30,7 +40,7 @@ describe('Login Controller', () => {
         const response = await sut.handle(request)
         expect(response).toEqual(missingParam('email'))
     })
-    test('should 400 if no username is provided', async () => {
+    test('should return 400 if no username is provided', async () => {
         const { sut } = makeSut()
         const request: HttpRequest = {
             params: {
@@ -41,7 +51,7 @@ describe('Login Controller', () => {
         const response = await sut.handle(request)
         expect(response).toEqual(missingParam('password'))
     })
-    test('should 400 if invalid email', async () => {
+    test('should return 400 if invalid email', async () => {
         const { sut, emailValidator } = makeSut()
         jest.spyOn(emailValidator, 'validate').mockResolvedValueOnce({ is_valid: false })
         const request: HttpRequest = {
@@ -53,5 +63,24 @@ describe('Login Controller', () => {
         }
         const response = await sut.handle(request)
         expect(response).toEqual(invalidParam('email'))
+    })
+    test('should return 400 if invalid credentails', async () => {
+        const { sut, collectionStub } = makeSut()
+
+        jest.spyOn(collectionStub, 'findOne').mockImplementationOnce(
+            async (_) => {
+                return null
+            }
+        )
+        
+        const request: HttpRequest = {
+            params: {
+                email: 'invalid-email',
+                password: 'invalid-password'
+            },
+            body: {}
+        }        
+        const response = await sut.handle(request)
+        expect(response).toEqual(invalidCredentials())
     })
 })
