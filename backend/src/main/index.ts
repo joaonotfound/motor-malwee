0644e129e137d5f1ef191c191175dc802dd39904
joaonotfound <joaonotfound@protonmail.com>
2022-10-22 21:59:07 -0300

    fix: ensure app use json

--- backend/src/main/index.ts
+++ backend/src/main/index.ts
@@ -1,8 +1,10 @@
 import { setupRoutes } from "./config";
 import { makeApp } from "./factory/app";
+import * as express from 'express'
 
 async function main(){
   const app = makeApp()
+  app.use(express.json())  
   await setupRoutes(app)
   app.listen(5000, () => {
     console.log('listening on localhost:5000')
