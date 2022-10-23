96eba9908df9f8db98e22edc029bdbc22d6a4c73
joaonotfound <joaonotfound@protonmail.com>
2022-10-22 21:11:43 -0300

    feat: ensure provided email is valid on login controller

--- backend/src/presentation/controllers/authentication/login.ts
+++ backend/src/presentation/controllers/authentication/login.ts
@@ -1,11 +1,22 @@
+import { EmailValidator } from "@/domain";
 import { Get, RequiredParams } from "@/presentation/decorators";
-import { ok } from "@/presentation/helpers";
+import { invalidParam, ok } from "@/presentation/helpers";
 import { HttpRequest } from "@/presentation/protocols";
 
 @Get('/auth/login')
 export class LoginController {
+    constructor(
+        private readonly emailValidator: EmailValidator
+    ){}
     @RequiredParams('email', 'password')
-    async handle(_: HttpRequest){
+    async handle(request: HttpRequest){
+        const { email } = request.params
+        
+        const validation = await this.emailValidator.validate(email)
+        if(!validation.is_valid){
+            return invalidParam('email')
+        }
+
         return ok({ message: 'login succesfully'})
     }
 }
