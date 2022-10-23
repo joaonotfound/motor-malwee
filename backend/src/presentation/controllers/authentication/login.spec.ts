0270de8331b7f56c56207aa84a940bd194047d52
joaonotfound <joaonotfound@protonmail.com>
2022-10-22 21:11:50 -0300

    test: ensure provided email is valid on login controller

--- backend/src/presentation/controllers/authentication/login.spec.ts
+++ backend/src/presentation/controllers/authentication/login.spec.ts
@@ -1,10 +1,21 @@
-import { missingParam } from "@/presentation/helpers"
+import { missingParam, invalidParam } from "@/presentation/helpers"
 import { HttpRequest } from "@/presentation/protocols"
 import { LoginController } from "./login"
+import { EmailValidator } from "@/domain"
+
+const makeEmailValidatorStub = () => {
+    class EmailValidatorStub implements EmailValidator {  
+        async validate(_: string) {
+            return { is_valid: true }    
+        }
+    }
+    return new EmailValidatorStub()
+}
 
 const makeSut = () => {
-    const sut = new LoginController()
-    return { sut }
+    const emailValidator = makeEmailValidatorStub()
+    const sut = new LoginController(emailValidator)
+    return { sut, emailValidator }
 }
 
 describe('Login Controller', () => {
@@ -29,5 +40,18 @@ describe('Login Controller', () => {
         }
         const response = await sut.handle(request)
         expect(response).toEqual(missingParam('password'))
+    })
+    test('should 400 if invalid email', async () => {
+        const { sut, emailValidator } = makeSut()
+        jest.spyOn(emailValidator, 'validate').mockResolvedValueOnce({ is_valid: false })
+        const request: HttpRequest = {
+            params: {
+                email: 'invalid-email',
+                password: 'valid-password'
+            },
+            body: {}
+        }
+        const response = await sut.handle(request)
+        expect(response).toEqual(invalidParam('email'))
     })
 })
