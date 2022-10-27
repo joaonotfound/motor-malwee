2596b205a080de381013bb99cbc8b9d2722d2bad
joaonotfound <joaonotfound@protonmail.com>
2022-10-27 14:27:24 -0300

    feat: create table component

--- frontend/src/app/components/table/table.component.ts
+++ frontend/src/app/components/table/table.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit , Output, EventEmitter} from '@angular/core';
+
+export type Column = { columnName: string, propertyName: string }
 
 @Component({
   selector: 'app-table',
@@ -6,10 +8,15 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
-
-  @Input() data: any
+  @Input() contentName: string = ''
+  @Output() onCreate = new EventEmitter()
+  @Input() columns: Column[] = []
+  @Input() data: any = []
   constructor() { }
 
+  getDisplayedColumns(){
+    return this.columns.map(column => column.propertyName)
+  }
   ngOnInit(): void {
   
   }
