c0dd2a6185ac9935d77ca496bd8592d235d1bb08
joaonotfound <joaonotfound@protonmail.com>
2022-10-27 16:19:16 -0300

    feat: should return 400 if group doesnt exists

--- backend/src/presentation/controllers/subgroups/create-sub-group.ts
+++ backend/src/presentation/controllers/subgroups/create-sub-group.ts
@@ -1,11 +1,22 @@
+import { groupEntity, Repository } from "@/domain";
 import { Post, RequiredParams } from "@/presentation/decorators";
+import { invalidParam, ok } from "@/presentation/helpers";
 import { HttpRequest } from "@/presentation/protocols";
 
 
 @Post('/subgroups')
 export class CreateSubGroupController {
+    constructor(
+        private readonly repository: Repository
+    ){}
     @RequiredParams('description', 'group')
-    async handle(_: HttpRequest){
-        
+    async handle(request: HttpRequest){
+        const { group } = request.params
+        const match_group = await this.repository.collection(groupEntity).findOne({ description: group })
+        if(!match_group){
+            return invalidParam('group')
+        }
+
+        return ok({ created: true })
     }
 }
