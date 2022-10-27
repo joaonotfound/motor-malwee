ed209ea5cbd5a2e81c37360ff6097ec1f2ef4a61
joaonotfound <joaonotfound@protonmail.com>
2022-10-27 14:28:30 -0300

    feat: create groups screen

--- frontend/src/app/groups/groups.component.ts
+++ frontend/src/app/groups/groups.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Column } from '../components/table/table.component';
+import { CreateGroupModalComponent } from '../modals/create-group-modal/create-group-modal.component';
+import { Groups, GroupsService } from '../services/rests/groups.service';
 
 @Component({
   selector: 'app-groups',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupsComponent implements OnInit {
 
-  constructor() { }
+  groups: Groups = []
+  table_columns: Column[] = [{ columnName: 'Descrição', propertyName: "description" }]
 
-  ngOnInit(): void {
+  constructor( 
+    private readonly dialog: MatDialog,
+    private readonly groupsService: GroupsService
+  ) {
+    this.groupsService.groups.subscribe(groups => this.groups = groups )
+    this.groupsService.loadGroups()
   }
+  openCreateModal(){
+    const dialogRef = this.dialog.open(CreateGroupModalComponent)
+    
+    dialogRef.afterClosed().subscribe(async response => {
+      if(response){
+        const created = await this.groupsService.createGroup(response)
+        if(created){
+          this.groupsService.loadGroups()
+        }
+      }
+    });
+  }
+  ngOnInit(): void {}
 
 }
