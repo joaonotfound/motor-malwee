90d8107a817de9a521d987953d11fa6988ca7e01
joaonotfound <joaonotfound@protonmail.com>
2022-10-26 21:53:01 -0300

    test: should return 400 if the group already exists

--- backend/src/presentation/controllers/groups/create-group.spec.ts
+++ backend/src/presentation/controllers/groups/create-group.spec.ts
@@ -1,10 +1,12 @@
-import { missingParam } from "@/presentation/helpers";
+import { alreadyInUse, missingParam } from "@/presentation/helpers";
 import { HttpRequest } from "@/presentation/protocols";
 import { CreateGroupController } from "./create-group";
+import { createRepositoryStub } from "@/presentation/helpers";
 
 const makeSut = () => {
-    const sut = new CreateGroupController();
-    return { sut }
+    const { repositoryStub, collectionStub } = createRepositoryStub()
+    const sut = new CreateGroupController(repositoryStub);
+    return { sut, repositoryStub, collectionStub }
 
 }
 describe('Create Group', () => {
@@ -16,5 +18,18 @@ describe('Create Group', () => {
         }
         const response = await sut.handle(request)
         expect(response).toEqual(missingParam('description'))
+    })
+    it('should return 400 if the group already exists', async () => {
+        const { sut, collectionStub } = makeSut()
+        jest.spyOn(collectionStub, 'findOne').mockResolvedValueOnce(true)
+
+        const request: HttpRequest = {
+            body: {
+                description: "Already-in-use-username"
+            },
+            params: {}
+        }
+        const response = await sut.handle(request)
+        expect(response).toEqual(alreadyInUse('description'))
     })
 })
