92181982282a786882082145ea998cb9ee9a7602
joaonotfound <joaonotfound@protonmail.com>
2022-10-27 16:19:23 -0300

    test: should return 400 if group doesnt exists

--- backend/src/presentation/controllers/subgroups/create-sub-group.spec.ts
+++ backend/src/presentation/controllers/subgroups/create-sub-group.spec.ts
@@ -1,10 +1,11 @@
-import { missingParam } from "@/presentation/helpers"
+import { createRepositoryStub, invalidParam, missingParam } from "@/presentation/helpers"
 import { HttpRequest } from "@/presentation/protocols"
 import { CreateSubGroupController } from "./create-sub-group"
 
 const makeSut = () => {
-    const sut = new CreateSubGroupController()
-    return { sut }
+    const { repositoryStub, collectionStub } = createRepositoryStub()
+    const sut = new CreateSubGroupController(repositoryStub)
+    return { sut, repositoryStub, collectionStub }
 }
 
 describe('CreateSubGroupController', () => {
@@ -29,5 +30,18 @@ describe('CreateSubGroupController', () => {
         }
         const response = await sut.handle(request)
         expect(response).toEqual(missingParam('group'))
+    })
+    it('should return 400 if group doesnt exists', async () => {
+        const { sut, collectionStub } = makeSut()
+        jest.spyOn(collectionStub, 'findOne').mockResolvedValueOnce(false)
+        const request: HttpRequest = {
+            body: {
+                group: 'invalid-group',
+                description: 'valid-description'
+            },
+            params: {}
+        }
+        const response = await sut.handle(request)
+        expect(response).toEqual(invalidParam('group'))
     })
 })
