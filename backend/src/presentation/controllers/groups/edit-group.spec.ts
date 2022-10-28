14204141984529a88504389f516b58d9296b1338
joaonotfound <joaonotfound@protonmail.com>
2022-10-28 15:23:40 -0300

    test: should return 400 if no group doesnt exist

--- backend/src/presentation/controllers/groups/edit-group.spec.ts
+++ backend/src/presentation/controllers/groups/edit-group.spec.ts
@@ -1,10 +1,11 @@
-import { missingParam } from "@/presentation/helpers"
+import { createRepositoryStub, invalidParam, missingParam } from "@/presentation/helpers"
 import { HttpRequest } from "@/presentation/protocols"
 import { EditGroupController } from "./edit-group"
 
 const makeSut = () => {
-    const sut = new EditGroupController()
-    return { sut }    
+    const { repositoryStub, collectionStub } = createRepositoryStub()
+    const sut = new EditGroupController(repositoryStub)
+    return { sut, repositoryStub, collectionStub }    
 }
 describe('EditGroupController', () => {
     it('should return 400 if no description was provided', async () => {
@@ -15,5 +16,18 @@ describe('EditGroupController', () => {
         }
         const response = await sut.handle(request)
         expect(response).toEqual(missingParam('description'))
+    })
+    it('should return 400 if no group doesnt exist', async () => {
+        const { sut, collectionStub } = makeSut()
+        jest.spyOn(collectionStub, 'findOne').mockResolvedValueOnce(false)
+        
+        const request: HttpRequest = {
+            params: {},
+            body: {
+                description: 'invalid-description'
+            }
+        }
+        const response = await sut.handle(request)
+        expect(response).toEqual(invalidParam('description'))
     })
 })
