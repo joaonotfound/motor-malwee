693beb155cab48d3e643c46af731dfbd019190dc
joaonotfound <joaonotfound@protonmail.com>
2022-10-28 16:06:50 -0300

    feat: create collection service

---
+++ frontend/src/app/services/rests/collections.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { createAxios } from 'src/helpers/create-axios';
+import { AuthenticationService } from '../authentication.service';
+
+export type Collection = { description: string }
+export type Collections = Array<Collection>
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CollectionsService {
+  private readonly _collections = new Subject<Collections>()
+  
+  constructor(
+    private readonly auth: AuthenticationService
+  ){}
+  
+  get collections(){ 
+    return this._collections.asObservable()
+  }
+
+  public async create(collection: Collection): Promise<boolean> {
+    const axios = createAxios(this.auth.getToken())
+    const response = await axios.post('/collections', collection)
+    if(response.data.created){
+      return true
+    }
+    return false
+  }
+
+  public async edit(previous_collection: Collection, new_collection: Collection){
+    const axios = createAxios(this.auth.getToken())
+    await axios.put('/collections', { collection: previous_collection.description, new_collection })
+    this.load()
+  }
+
+  public async load(){
+    const axios = createAxios(this.auth.getToken())  
+    const response = await axios.get('/collections')
+    const collections = response.data.collections
+    if(collections){
+      this._collections.next(collections)
+    }
+
+  }
+}
