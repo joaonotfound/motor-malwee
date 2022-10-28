cbba3b0896021cae1a3193990a277deac203b685
joaonotfound <joaonotfound@protonmail.com>
2022-10-28 16:18:08 -0300

    fix: add Collection to orm entities list

--- backend/src/mikro-orm.config.ts
+++ backend/src/mikro-orm.config.ts
@@ -1,12 +1,12 @@
 import {  EntityCaseNamingStrategy, Configuration, Options, IDatabaseDriver, Connection } from '@mikro-orm/core'
 
-import { Group, SubGroup, User } from 'infra/mikro-orm/entities';
+import { Group, SubGroup, User, Collection } from 'infra/mikro-orm/entities';
 import * as dotenv from 'dotenv';
 
 dotenv.config()
 
 const config: Configuration<IDatabaseDriver<Connection>> | Options<IDatabaseDriver<Connection>> = {
-    entities: [ User, Group, SubGroup ],
+    entities: [ User, Group, SubGroup, Collection ],
     type: 'mysql',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'root',
