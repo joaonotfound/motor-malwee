f768ca9cdeca8d1eab4998aeca9a5c262afccf36
joaonotfound <joaonotfound@protonmail.com>
2022-10-28 16:18:35 -0300

    feat: add table to collections page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CollectionsComponent } from './collections/collections.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { GroupsComponent } from './groups/groups.component';
 import { LoginComponent } from './login/login.component';
@@ -10,7 +11,8 @@ const routes: Routes = [
   { path: 'signup', component: SignupComponent },
   { path: 'dashboard', component: DashboardComponent,
           children: [
-            { path: 'groups', component: GroupsComponent }
+            { path: 'groups', component: GroupsComponent },
+            { path: "collections", component: CollectionsComponent }
           ]
   }
 ];
--- frontend/src/app/collections/collections.component.ts
+++ frontend/src/app/collections/collections.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Collection, Collections, CollectionsService } from '../services/rests/collections.service';
+import { Column } from '../components/table/table.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-collections',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsComponent implements OnInit {
 
-  constructor() { }
+  collections: Collections = []
+  table_columns: Column[] = [{ columnName: 'Descrição', propertyName: "description" }]
 
-  ngOnInit(): void {
+  constructor( 
+    // private readonly dialog: MatDialog,
+    private readonly collectionsService: CollectionsService
+  ) {
+    this.collectionsService.collections.subscribe(collections => this.collections = collections )
+    this.collectionsService.load()
   }
+  // openEditModal(collection: Collection) {
+  //   const dialogRef = this.dialog.open(EditGroupModalComponent, { data: group, width: '600px' })
+    
+  //   dialogRef.afterClosed().subscribe(async response => {
+  //     if(response){
+  //       const { previous_group, new_group } = response
+  //       this.collectionsService.edit(previous_group, new_group)
+  //     }
+  //   });
+  // }
+  // filter(query: string){
+  //   console.log(query)
+  // }
+
+  // openCreateModal(){
+  //   const dialogRef = this.dialog.open(CreateGroupModalComponent, { width: '400px' })
+    
+  //   dialogRef.afterClosed().subscribe(async response => {
+  //     if(response){
+  //       const created = await this.collectionsService.createGroup(response)
+  //       if(created){
+  //         this.collectionsService.loadGroups()
+  //       }
+  //     }
+  //   });
+  // }
+  ngOnInit(): void {}
 
 }
