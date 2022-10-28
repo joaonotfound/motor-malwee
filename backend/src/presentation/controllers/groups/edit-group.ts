c93eda55170790cf961e7f8f85ea6a1880438e19
joaonotfound <joaonotfound@protonmail.com>
2022-10-28 15:23:34 -0300

    feat: should return 400 if no group doesnt exist

--- backend/src/presentation/controllers/groups/edit-group.ts
+++ backend/src/presentation/controllers/groups/edit-group.ts
@@ -1,11 +1,22 @@
+import { groupEntity, Repository } from "@/domain";
 import { Put, RequiredParams } from "@/presentation/decorators";
+import { invalidParam, ok } from "@/presentation/helpers";
 import { HttpRequest } from "@/presentation/protocols";
 
 @Put('/groups')
 export class EditGroupController {
-    @RequiredParams(['description'])
-    async handle(_: HttpRequest){
-        
+    constructor(
+        private readonly repository: Repository
+    ){}
+
+    @RequiredParams(['description'], { on: 'body'})
+    async handle(request: HttpRequest){
+        const { description } = request.body
+        const match_group = await this.repository.collection(groupEntity).findOne({ description })
+        if(!match_group){
+            return invalidParam('description')
+        }
+        return ok({ edited: true )
     }
 
 }
