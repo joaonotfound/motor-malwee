00a48c9ed2531061b2e6c3b2597f009f7ad7387f
joaonotfound <joaonotfound@protonmail.com>
2022-10-29 13:18:50 -0300

    feat: should return 400 if group doesnt exists

--- backend/src/presentation/controllers/products/create-product.ts
+++ backend/src/presentation/controllers/products/create-product.ts
@@ -1,10 +1,21 @@
+import { groupEntity, Repository } from "@/domain";
 import { Post, RequiredParams } from "@/presentation/decorators";
+import { invalidParam } from "@/presentation/helpers";
 import { HttpRequest } from "@/presentation/protocols";
 
 @Post('/products')
 export class CreateProductController {
-    @RequiredParams(['description', 'price', 'subgroup', 'collection'])
+    constructor(
+        private readonly repository: Repository
+    ){}
+    @RequiredParams(['description', 'price', 'group', 'subgroup', 'collection'])
     async handle(request: HttpRequest){
+        const { group } = request.body
+        const match_group = await this.repository.collection(groupEntity).findOne({ description: group })
+        if(!match_group){
+            return invalidParam('group')
+        }
+        // const match_product = await this.repository.collection(productsEntity).findOne({ fk_subgroup:})
         return request
     }
 }
