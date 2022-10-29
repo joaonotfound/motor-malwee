843e35c56cc507498695e27083d79709bec11650
joaonotfound <joaonotfound@protonmail.com>
2022-10-29 14:38:40 -0300

    test: should return the list of products

---
+++ backend/src/presentation/controllers/products/load-products.spec.ts
@@ -0,0 +1,17 @@
+import { createRepositoryStub } from "@/presentation/helpers"
+import { LoadProductsController } from "./load-products"
+
+const makeSut = () => {
+    const { repositoryStub, collectionStub } = createRepositoryStub()
+    const sut = new LoadProductsController(repositoryStub)
+    return { sut, repositoryStub, collectionStub }
+}
+
+describe('LoadProductsController', () => {
+    it('should return the list of products', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.handle()
+        expect(response).toEqual([])
+    })
+})
