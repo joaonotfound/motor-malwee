6ba97721d00c1c56589aad22f10e413a98c53892
joaonotfound <joaonotfound@protonmail.com>
2022-10-29 15:43:54 -0300

    chore: implement products

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { CollectionsComponent } from './pages/collections/collections.component'
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { GroupsComponent } from './pages/groups/groups.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ProductsComponent } from './pages/products/products.component';
 import { SignupComponent } from './pages/signup/signup.component';
 
 const routes: Routes = [
@@ -12,7 +13,8 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent,
           children: [
             { path: 'groups', component: GroupsComponent },
-            { path: "collections", component: CollectionsComponent }
+            { path: "collections", component: CollectionsComponent },
+            { path: "products", component: ProductsComponent }
           ]
   }
 ];
--- frontend/src/app/pages/products/products.component.ts
+++ frontend/src/app/pages/products/products.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Column } from 'src/app/components/table/table.component';
+import { Product } from 'src/app/models/entities/product';
+import { ProductsService } from 'src/app/services/rests/products';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = []
+  table_columns: Column[] = [
+    { columnName: 'Descrição', propertyName: "description" },
+    { columnName: 'Grupo', propertyName: "group" },
+    { columnName: 'Subgrupo', propertyName: "subgroup" },
+    { columnName: 'Coleção', propertyName: "collection" }
+  ]
 
-  ngOnInit(): void {
+  constructor( 
+    private readonly productsService: ProductsService
+  ) {
+    this.loadProducts()
   }
+
+  private async loadProducts(){
+    this.products = await this.productsService.load()
+  }
+
+  ngOnInit(): void {}
 
 }
