ab1329b98fd2b06a0ddefa902732f06b64fc7a3e
joaonotfound <joaonotfound@protonmail.com>
2022-10-29 13:06:45 -0300

    test: should return 400 if is missing params

--- backend/src/presentation/controllers/products/create-product.spec.ts
+++ backend/src/presentation/controllers/products/create-product.spec.ts
@@ -21,4 +21,43 @@ describe('CreteProductController', () => {
         const response = await sut.handle(request)
         expect(response).toEqual(missingParam('description'))
     })
+    it('should return 400 if no price is providen', async () => {
+        const { sut } = makeSut()
+        const request: HttpRequest = {
+            body: { 
+                description: 'valid-description',
+                subgroup: 'valid-subgroup',
+                collection: 'valid-collection'
+            }, 
+            params: {}
+        }
+        const response = await sut.handle(request)
+        expect(response).toEqual(missingParam('price'))
+    })
+    it('should return 400 if no subgroup is providen', async () => {
+        const { sut } = makeSut()
+        const request: HttpRequest = {
+            body: { 
+                description: 'valid-description',
+                price: 'valid-price',                
+                collection: 'valid-collection'
+            }, 
+            params: {}
+        }
+        const response = await sut.handle(request)
+        expect(response).toEqual(missingParam('subgroup'))
+    })
+    it('should return 400 if no collection is providen', async () => {
+        const { sut } = makeSut()
+        const request: HttpRequest = {
+            body: { 
+                description: 'valid-description',
+                price: 'valid-price',                
+                subgroup: 'valid-subgroup'
+            }, 
+            params: {}
+        }
+        const response = await sut.handle(request)
+        expect(response).toEqual(missingParam('collection'))
+    })
 })
