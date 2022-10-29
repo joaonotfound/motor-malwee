a6008a5a1290e77d3e390016d6a898985d147e73
joaonotfound <joaonotfound@protonmail.com>
2022-10-29 17:53:14 -0300

    feat: should return 400 if already exists an customer with the same CPNJ

--- backend/src/presentation/controllers/customers/create-customer.ts
+++ backend/src/presentation/controllers/customers/create-customer.ts
@@ -1,10 +1,20 @@
+import { customerEntity, Repository } from "@/domain";
 import { RequiredParams } from "@/presentation/decorators";
+import { invalidParam } from "@/presentation/helpers";
 import { HttpRequest } from "@/presentation/protocols";
 
 export class CreateCustomerController{
-    
+    constructor(
+        private readonly repository: Repository
+    ){}
     @RequiredParams(['popularName', 'CPNJ', 'companyName'])
     async handle(request: HttpRequest){
+        const { CPNJ } = request.body
+        let match_user = await this.repository.collection(customerEntity).find({ CPNJ }) 
+        if(match_user){
+            return invalidParam('CPNJ')
+        }
+        
         return request
     }
 }
