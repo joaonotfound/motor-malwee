5719b0ca7d58fb23f9436f2043fc68fcabd2ae88
joaonotfound <joaonotfound@protonmail.com>
2022-10-29 15:17:57 -0300

    feat: should return the list of products

--- backend/src/presentation/controllers/products/load-products.ts
+++ backend/src/presentation/controllers/products/load-products.ts
@@ -1,13 +1,24 @@
-import { productsEntity, Repository } from "@/domain";
+import { Repository } from "@/domain";
+import { Product } from "@/infra";
 import { Get } from "@/presentation/decorators";
+import { ok } from "@/presentation/helpers";
+import { HttpResponse } from "@/presentation/protocols";
 
 @Get('/products')
 export class LoadProductsController {
     constructor(
         private readonly repository: Repository
     ){}
-    async handle(){
-        const products = await this.repository.collection(productsEntity).find({})
-        return products
+    async handle(): Promise<HttpResponse<Array<Product>>>{
+        const products = await this.repository.execute(getSelectQuery())
+        return ok(products)
     }
+}
+
+function getSelectQuery(): string {
+    return "select p.description, p.price, c.description as collection, sb.description as subgroup, g.description as `group` "+
+        "from product as p "+
+        "left join collection as c on p.fk_collection = c.id "+
+        "left join subgroup as sb on p.fk_subgroup = sb.id "+
+        "left join `group` as g on sb.fk_group = g.id "
 }
