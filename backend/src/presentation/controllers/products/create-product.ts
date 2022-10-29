24d1803b39a608a05d0162b151b6954c30e27c24
joaonotfound <joaonotfound@protonmail.com>
2022-10-29 13:44:20 -0300

    feat: should return 400 is price is not a number

--- backend/src/presentation/controllers/products/create-product.ts
+++ backend/src/presentation/controllers/products/create-product.ts
@@ -1,4 +1,4 @@
-import { collectionEntity, groupEntity, Repository, subGroupEntity } from "@/domain";
+import { collectionEntity, groupEntity, productsEntity, Repository, subGroupEntity } from "@/domain";
 import { Post, RequiredParams } from "@/presentation/decorators";
 import { invalidParam } from "@/presentation/helpers";
 import { HttpRequest } from "@/presentation/protocols";
@@ -10,20 +10,32 @@ export class CreateProductController {
     ){}
     @RequiredParams(['description', 'price', 'group', 'subgroup', 'collection'])
     async handle(request: HttpRequest){
-        const { group, subgroup, collection } = request.body
+        const { group, subgroup, collection, description, price } = request.body
+
         const match_group = await this.repository.collection(groupEntity).findOne({ description: group })
         if(!match_group){
             return invalidParam('group')
         }
+
         const match_subgroup = await this.repository.collection(subGroupEntity).findOne({ fk_group: match_group.id, description: subgroup })
         if(!match_subgroup){
             return invalidParam('subgroup')
         }
+
         const match_collection = await this.repository.collection(collectionEntity).findOne({ description: collection })
         if(!match_collection){
             return invalidParam('collection')
         }
-        // const match_product = await this.repository.collection(productsEntity).findOne({ fk_subgroup:})
+
+        const match_product =  await this.repository.collection(productsEntity).findOne({ description, fk_collection: collection, fk_subgroup: match_subgroup.id })
+        if(match_product){
+            return invalidParam('description')
+        }
+
+        if(typeof price !== 'number' ){
+            return invalidParam('price')
+        }
+        
         return request
     }
 }
