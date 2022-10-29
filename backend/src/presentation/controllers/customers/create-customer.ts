e9e3e0ad7762b65c51b3cfaa43271376ab000f3b
joaonotfound <joaonotfound@protonmail.com>
2022-10-29 18:03:32 -0300

    feat: should return 200 if the user was created

--- backend/src/presentation/controllers/customers/create-customer.ts
+++ backend/src/presentation/controllers/customers/create-customer.ts
@@ -1,6 +1,6 @@
 import { customerEntity, Repository } from "@/domain";
 import { RequiredParams } from "@/presentation/decorators";
-import { invalidParam } from "@/presentation/helpers";
+import { invalidParam, ok } from "@/presentation/helpers";
 import { HttpRequest } from "@/presentation/protocols";
 
 export class CreateCustomerController{
@@ -9,12 +9,17 @@ export class CreateCustomerController{
     ){}
     @RequiredParams(['popularName', 'CPNJ', 'companyName'])
     async handle(request: HttpRequest){
-        const { CPNJ } = request.body
-        let match_user = await this.repository.collection(customerEntity).find({ CPNJ }) 
+        const { CPNJ, popularName, companyName } = request.body
+        
+        let match_user = await this.repository.collection(customerEntity).findOne({ CPNJ }) 
         if(match_user){
             return invalidParam('CPNJ')
         }
-        
-        return request
+
+        const customer = { CPNJ, companyName, popularName }
+
+        await this.repository.collection(customerEntity).save(customer)
+
+        return ok({ created: true, customer })
     }
 }
