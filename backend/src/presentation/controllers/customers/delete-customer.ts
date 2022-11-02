789adc41c39a8de4de0e92f137ee94e12cabf75f
joaonotfound <joaonotfound@protonmail.com>
2022-11-02 17:53:47 -0300

    feat: should return invalid param if customer not found

--- backend/src/presentation/controllers/customers/delete-customer.ts
+++ backend/src/presentation/controllers/customers/delete-customer.ts
@@ -1,3 +1,6 @@
+import { invalidParam } from '@/presentation/helpers';
+import { customerEntity } from './../../../domain/entities/customer';
+import { Repository } from '@/domain';
 import { ok } from '@/presentation/helpers';
 import { HttpRequest } from './../../protocols';
 import { Del, RequiredParams } from "@/presentation/decorators";
@@ -6,8 +9,17 @@ import { Del, RequiredParams } from "@/presentation/decorators";
 
 @Del('/customers')
 export class DeleteCustomerController{
+    constructor( private readonly repository: Repository ){}
+
     @RequiredParams(['customer'])
     async handle(request: HttpRequest){
+        const { customer } = request.body
+
+        const deletedCustomer = await this.repository.collection(customerEntity).deactivate({ CPNJ: customer })
+        if(!deletedCustomer){
+            return invalidParam('customer')
+        }
+        
         return ok(request)
     }
 }
