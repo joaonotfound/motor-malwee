6a54433e99fa40c2b27ae148480f137c59b88780
joaonotfound <joaonotfound@protonmail.com>
2022-11-02 17:23:48 -0300

    feat: should return invalid param if collection not found

--- backend/src/presentation/controllers/collections/delete-collection.ts
+++ backend/src/presentation/controllers/collections/delete-collection.ts
@@ -1,11 +1,27 @@
+import { invalidParam } from '@/presentation/helpers';
+import { collectionEntity } from './../../../domain/entities/collection-entity';
+import { Repository } from '@/domain';
 import { ok } from '@/presentation/helpers';
 import { HttpRequest } from './../../protocols';
 import { Del, RequiredParams } from "@/presentation/decorators";
 
 @Del('/collections')
 export class DeleteCollectionController {
-    @RequiredParams(['description'])
-    async handle(request: HttpRequest){
-        return ok(request)
+    constructor(private readonly repository: Repository) { }
+
+    @RequiredParams(['collection'])
+    async handle(request: HttpRequest) {
+
+        const { collection } = request.body
+        const deletedEntity = await this.repository
+            .collection(collectionEntity)
+            .delete({ description: collection })
+        
+        if(!deletedEntity){
+            return invalidParam('collection')
+        }
+
+        return ok(deletedEntity)
+
     }
 }
