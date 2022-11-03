903e15e0ca5eae5a3831221b7e9a334208c30d0e
joaonotfound <joaonotfound@protonmail.com>
2022-11-03 13:29:49 -0300

    feat: should return 400 if group doesnt exists

--- backend/src/presentation/controllers/subgroups/delete-sub-group.ts
+++ backend/src/presentation/controllers/subgroups/delete-sub-group.ts
@@ -1,10 +1,18 @@
+import { groupEntity, Repository } from "@/domain";
 import { Del, RequiredParams } from "@/presentation/decorators";
+import { invalidParam, ok } from "@/presentation/helpers";
 import { HttpRequest } from "@/presentation/protocols";
 
 @Del('/subgroups')
 export class DeleteSubgroupController {
+    constructor( private readonly repository: Repository ){}
     @RequiredParams(['description', 'group'])
     async handle(request: HttpRequest){
-        console.log(request)
+        const { description } = request.body
+        const match_group = await this.repository.collection(groupEntity).findOne({ description })
+        if(!match_group){
+            return invalidParam('group')
+        }
+        return ok(request)
     }
 }
