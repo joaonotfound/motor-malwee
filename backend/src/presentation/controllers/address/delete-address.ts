bcc44f5140d25b0917d29f68a1eff687be2d9263
joaonotfound <joaonotfound@protonmail.com>
2022-11-09 16:38:19 -0300

    feat: should call decode method

--- backend/src/presentation/controllers/address/delete-address.ts
+++ backend/src/presentation/controllers/address/delete-address.ts
@@ -1,10 +1,16 @@
+import { addressEntity, HashID, Repository } from "@/domain";
 import { Del, RequiredParams } from "@/presentation/decorators";
+import { ok } from "@/presentation/helpers";
 import { HttpRequest } from "@/presentation/protocols";
 
 @Del('/address')
 export class DeleteAddressController {
+    constructor( private readonly idHasher: HashID, private readonly repository: Repository ){}
     @RequiredParams(['id'])
     async handle(request: HttpRequest){
-        return request
-    }
+        const { id } = request.body
+        const private_id = this.idHasher.decode(id)
+        await this.repository.collection(addressEntity).deactivate({ id: private_id})
+        return ok({ deleted: true })
+    }   
 }
