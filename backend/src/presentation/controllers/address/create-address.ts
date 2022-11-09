abb8c958fd7735744aaec6134a8e96403d70c898
joaonotfound <joaonotfound@protonmail.com>
2022-11-09 13:29:14 -0300

    feat: should return 400 if no user was providen

--- backend/src/presentation/controllers/address/create-address.ts
+++ backend/src/presentation/controllers/address/create-address.ts
@@ -1,10 +1,12 @@
+import { Address } from "@/domain";
 import { RequiredParams } from "@/presentation/decorators";
 import { HttpRequest } from "@/presentation/protocols";
 
 
 export class CreateAddressController{
-    @RequiredParams(['street', 'city', 'state', 'country', 'district'])
-    async handle(request: HttpRequest){
+    @RequiredParams(['street', 'city', 'state', 'country', 'district', 'user'])
+    async handle(request: HttpRequest<Partial<Address>>){
         console.log(request)
+        
     }
 }
