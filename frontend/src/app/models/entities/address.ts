72cd0c419f81c064ca26b75a269da66316bd9807
joaonotfound <joaonotfound@protonmail.com>
2022-11-09 15:02:36 -0300

    feat: create Address model on client

---
+++ frontend/src/app/models/entities/address.ts
@@ -0,0 +1,12 @@
+
+export interface Address {
+    state: string,
+    customer: number,
+    city: string,
+    country: string,
+    street: string,
+    district: string,
+    reference?: string,
+    complement?: string,
+    zip?: string
+}
