784c9a4ec64b750ccd86be30e7dc48b56e32bf32
joaonotfound <joaonotfound@protonmail.com>
2022-11-09 13:44:38 -0300

    feat: save address into database

--- backend/src/presentation/controllers/address/create-address.ts
+++ backend/src/presentation/controllers/address/create-address.ts
@@ -1,4 +1,4 @@
-import { Address, HashID, Repository, userEntity } from "@/domain";
+import { Address, addressEntity, HashID, Repository, userEntity } from "@/domain";
 import { RequiredParams } from "@/presentation/decorators";
 import { invalidParam, ok } from "@/presentation/helpers";
 import { HttpRequest } from "@/presentation/protocols";
@@ -8,12 +8,15 @@ export class CreateAddressController{
     constructor( private readonly idHasher: HashID, private readonly repository: Repository  ){}
     @RequiredParams(['street', 'city', 'state', 'country', 'district', 'user'])
     async handle(request: HttpRequest<Partial<Address>>){
-        const { user } = request.body
+        const { user } = request.body 
+        const address = request.body
+
         const private_id = this.idHasher.decode(user)
         const matchUser = await this.repository.collection(userEntity).findOne({ id: private_id })
         if(!matchUser){
             return invalidParam('user')
         }
+        await this.repository.collection(addressEntity).save(address)
         return ok({ created: true })
         
     }
