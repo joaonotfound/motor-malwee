6a965a623a9416109ea5514e1d19b09f38a86433
joaonotfound <joaonotfound@protonmail.com>
2022-11-09 14:21:24 -0300

    feat: should call decode method

--- backend/src/presentation/controllers/address/load-address.ts
+++ backend/src/presentation/controllers/address/load-address.ts
@@ -1,10 +1,14 @@
+import { HashID } from "@/domain";
 import { Get, RequiredParams } from "@/presentation/decorators";
 import { HttpRequest } from "@/presentation/protocols";
 
 @Get('/address')
 export class LoadAddressController {
+    constructor( private readonly idHasher: HashID ){}
     @RequiredParams(['user'], { on: 'params' })
     async handle(request: HttpRequest){
-        return request
+        const { user } = request.params
+        const private_id = this.idHasher.decode(user)
+        return private_id
     }
 }
