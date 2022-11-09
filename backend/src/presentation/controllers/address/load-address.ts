eb1cfca41adfeafc9606284f49bbe19370e93a3e
joaonotfound <joaonotfound@protonmail.com>
2022-11-09 14:24:23 -0300

    feat: should return invalidParam if invlaid user

--- backend/src/presentation/controllers/address/load-address.ts
+++ backend/src/presentation/controllers/address/load-address.ts
@@ -1,14 +1,20 @@
-import { HashID } from "@/domain";
+import { HashID, Repository, userEntity } from "@/domain";
 import { Get, RequiredParams } from "@/presentation/decorators";
+import { invalidParam, ok } from "@/presentation/helpers";
 import { HttpRequest } from "@/presentation/protocols";
 
 @Get('/address')
 export class LoadAddressController {
-    constructor( private readonly idHasher: HashID ){}
+    constructor( private readonly idHasher: HashID, private readonly repository: Repository ){}
     @RequiredParams(['user'], { on: 'params' })
     async handle(request: HttpRequest){
         const { user } = request.params
         const private_id = this.idHasher.decode(user)
-        return private_id
+
+        const matchUser = await this.repository.collection(userEntity).findOne({ id: private_id })
+        if(!matchUser){
+            return invalidParam('user')
+        }
+        return ok({ address: [] })
     }
 }
