604682c55d19891f916bccb004791fceda66dfd7
joaonotfound <joaonotfound@protonmail.com>
2022-11-10 15:35:47 -0300

    feat: should return invalid address

--- backend/src/presentation/controllers/order/create-order.ts
+++ backend/src/presentation/controllers/order/create-order.ts
@@ -1,4 +1,4 @@
-import { customerEntity, HashID, Repository } from "@/domain";
+import { addressEntity, customerEntity, HashID, Repository } from "@/domain";
 import { Post, RequiredParams } from "@/presentation/decorators";
 import { invalidParam } from "@/presentation/helpers";
 import { HttpRequest } from "@/presentation/protocols";
@@ -9,12 +9,20 @@ export class CreateOrderController {
     constructor( private readonly encoder: HashID, private readonly repository: Repository ){}
     @RequiredParams(['customer', 'address'])
     async handle(request: HttpRequest){
-        const { customer } = request.body
+        const { customer, address } = request.body
         const customerPrivateId = this.encoder.decode(customer)
+        
         const matchCustomer = await this.repository.collection(customerEntity).findOne({ id: customerPrivateId })
         if(!matchCustomer){
             return invalidParam('customer')
         }
+        const addressPrivateId = this.encoder.decode(address)
+
+        const matchAddress = await this.repository.collection(addressEntity).findOne({ id: addressPrivateId })
+        if(!matchAddress){
+            return invalidParam('address')
+        }
+
         return request
     }
 }
