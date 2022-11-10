c1c5d6bd04b37afda23ffe21eaad8f78c99766c7
joaonotfound <joaonotfound@protonmail.com>
2022-11-10 15:34:20 -0300

    test: should return 400 if invalid customer

--- backend/src/presentation/controllers/order/create-order.spec.ts
+++ backend/src/presentation/controllers/order/create-order.spec.ts
@@ -1,11 +1,12 @@
-import { makeHashIDStub, missingParam } from "@/presentation/helpers"
+import { createRepositoryStub, invalidParam, makeHashIDStub, missingParam } from "@/presentation/helpers"
 import { HttpRequest } from "@/presentation/protocols"
 import { CreateOrderController } from "./create-order"
 
 const makeSut = () => {
+    const { repositoryStub, collectionStub} = createRepositoryStub()
     const hasherId = makeHashIDStub()
-    const sut = new CreateOrderController(hasherId)
-    return { sut, hasherId }
+    const sut = new CreateOrderController(hasherId, repositoryStub)
+    return { sut, hasherId, repositoryStub, collectionStub }
 }
 
 describe('CreateOrderController', () => {
@@ -41,5 +42,18 @@ describe('CreateOrderController', () => {
         }
         await sut.handle(request)
         expect(decodeSpy).toHaveBeenCalledWith('valid-customer')
+    })
+    it('should return 400 if invalid customer', async () => {
+        const { sut, collectionStub } = makeSut()
+        jest.spyOn(collectionStub, 'findOne').mockResolvedValueOnce(false)
+        const request: HttpRequest = {
+            body: { 
+                customer: 'valid-customer',
+                address: 'valid-address'
+            },
+            params: {}
+        }
+        const response = await sut.handle(request)
+        expect(response).toEqual(invalidParam('customer'))
     })
 })
