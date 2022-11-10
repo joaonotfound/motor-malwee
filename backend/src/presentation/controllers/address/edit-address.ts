ac3469b7695ea7376f48a7f096df4582736ad5d3
joaonotfound <joaonotfound@protonmail.com>
2022-11-10 13:52:33 -0300

    feat: should return 400 if invalid customer

--- backend/src/presentation/controllers/address/edit-address.ts
+++ backend/src/presentation/controllers/address/edit-address.ts
@@ -1,14 +1,20 @@
-import { HashID } from "@/domain";
+import { HashID, Repository, userEntity } from "@/domain";
 import { Put, RequiredParams } from "@/presentation/decorators";
+import { invalidParam } from "@/presentation/helpers";
 import { HttpRequest } from "@/presentation/protocols";
 
 @Put('/address')
 export class EditAddressController{
-    constructor( private readonly idHasher: HashID ){}
+    constructor( private readonly idHasher: HashID, private readonly repository: Repository ){}
     @RequiredParams(['customer'])
     async handle(request: HttpRequest){
         const { customer } = request.body
-        this.idHasher.decode(customer)
+        const privateId = this.idHasher.decode(customer)
+        const matchUser = await this.repository.collection(userEntity).findOne({ id: privateId })
+        if(!matchUser){
+            return invalidParam('customer')
+        }
         return request
+
     }
 }
