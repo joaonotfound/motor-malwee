b2d5d862c0b66a4c767ce08770a056dfc8746377
joaonotfound <joaonotfound@protonmail.com>
2022-11-10 13:50:08 -0300

    feat: should call decode

--- backend/src/presentation/controllers/address/edit-address.ts
+++ backend/src/presentation/controllers/address/edit-address.ts
@@ -1,10 +1,14 @@
+import { HashID } from "@/domain";
 import { Put, RequiredParams } from "@/presentation/decorators";
 import { HttpRequest } from "@/presentation/protocols";
 
 @Put('/address')
 export class EditAddressController{
+    constructor( private readonly idHasher: HashID ){}
     @RequiredParams(['customer'])
     async handle(request: HttpRequest){
+        const { customer } = request.body
+        this.idHasher.decode(customer)
         return request
     }
 }
