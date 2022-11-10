d33320ece8f8746cad6290727b2e23d0ad37d663
joaonotfound <joaonotfound@protonmail.com>
2022-11-10 13:52:39 -0300

    test: should return 400 if invalid customer

--- backend/src/presentation/controllers/address/edit-address.spec.ts
+++ backend/src/presentation/controllers/address/edit-address.spec.ts
@@ -1,11 +1,12 @@
-import { makeHashIDStub, missingParam } from "@/presentation/helpers"
+import { createRepositoryStub, invalidParam, makeHashIDStub, missingParam } from "@/presentation/helpers"
 import { HttpRequest } from "@/presentation/protocols"
 import { EditAddressController } from "./edit-address"
 
 const makeSut = () => {
     const idHasher = makeHashIDStub()
-    const sut = new EditAddressController(idHasher)
-    return { sut, idHasher }
+    const { repositoryStub, collectionStub } = createRepositoryStub()
+    const sut = new EditAddressController(idHasher, repositoryStub)
+    return { sut, idHasher, repositoryStub, collectionStub}
 }
 
 describe('EditAddressController', () => {
@@ -29,5 +30,16 @@ describe('EditAddressController', () => {
         }
         await sut.handle(request)
         expect(decodeSpy).toHaveBeenCalledWith('valid-customer')
+    })
+    it('should return 400 if invalid customer', async () => {
+        const { sut } = makeSut()
+        const request: HttpRequest = {
+            body: {
+                customer: 'invalid-customer'
+            },
+            params: {}
+        }
+        const response = await sut.handle(request)
+        expect(response).toEqual(invalidParam('customer'))
     })
 })
