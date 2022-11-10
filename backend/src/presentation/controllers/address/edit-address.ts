0da6916c98054326f43a44abb478c4923f0002b9
joaonotfound <joaonotfound@protonmail.com>
2022-11-10 14:10:13 -0300

    feat: should edit address on database

--- backend/src/presentation/controllers/address/edit-address.ts
+++ backend/src/presentation/controllers/address/edit-address.ts
@@ -1,4 +1,4 @@
-import { HashID, Repository, userEntity } from "@/domain";
+import { addressEntity, HashID, Repository, userEntity } from "@/domain";
 import { Put, RequiredParams } from "@/presentation/decorators";
 import { invalidParam } from "@/presentation/helpers";
 import { HttpRequest } from "@/presentation/protocols";
@@ -8,13 +8,23 @@ export class EditAddressController{
     constructor( private readonly idHasher: HashID, private readonly repository: Repository ){}
     @RequiredParams(['customer', 'id'])
     async handle(request: HttpRequest){
-        const { customer } = request.body
-        const privateId = this.idHasher.decode(customer)
-        const matchCustomer = await this.repository.collection(userEntity).findOne({ id: privateId })
+        const { customer, id } = request.body
+        const address = request.body
+
+        const customerPrivateId = this.idHasher.decode(customer)
+        const addressPrivateId = this.idHasher.decode(id)
+
+        const matchCustomer = await this.repository.collection(userEntity).findOne({ id: customerPrivateId })
         if(!matchCustomer){
             return invalidParam('customer')
+        }        
+        const matchAddress = await this.repository.collection(addressEntity).findOne({ id: addressPrivateId })
+        if(!matchAddress){
+            return invalidParam('address')
         }
-        // await this.repository.collection(userEntity).update(Object.assign({}, ))
+        
+        await this.repository.collection(userEntity).update(Object.assign({}, address, { customer: customerPrivateId, address: addressPrivateId } ))
+
         return request
 
     }
