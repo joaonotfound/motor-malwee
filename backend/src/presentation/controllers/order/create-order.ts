a36834b2f9ab1d0a97ea9bc3785fda1c62ce736b
joaonotfound <joaonotfound@protonmail.com>
2022-11-10 15:34:14 -0300

    feat: should return 400 if invalid customer

--- backend/src/presentation/controllers/order/create-order.ts
+++ backend/src/presentation/controllers/order/create-order.ts
@@ -1,15 +1,20 @@
-import { HashID } from "@/domain";
+import { customerEntity, HashID, Repository } from "@/domain";
 import { Post, RequiredParams } from "@/presentation/decorators";
+import { invalidParam } from "@/presentation/helpers";
 import { HttpRequest } from "@/presentation/protocols";
 
 
 @Post('/order')
 export class CreateOrderController {
-    constructor( private readonly encoder: HashID ){}
+    constructor( private readonly encoder: HashID, private readonly repository: Repository ){}
     @RequiredParams(['customer', 'address'])
     async handle(request: HttpRequest){
         const { customer } = request.body
-        this.encoder.decode(customer)
+        const customerPrivateId = this.encoder.decode(customer)
+        const matchCustomer = await this.repository.collection(customerEntity).findOne({ id: customerPrivateId })
+        if(!matchCustomer){
+            return invalidParam('customer')
+        }
         return request
     }
 }
