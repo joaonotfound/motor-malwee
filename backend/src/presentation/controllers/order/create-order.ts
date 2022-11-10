044d3d2b238c79a9c19f28fdd3dd8e48f0641d71
joaonotfound <joaonotfound@protonmail.com>
2022-11-10 15:29:44 -0300

    feat: should call decoder

--- backend/src/presentation/controllers/order/create-order.ts
+++ backend/src/presentation/controllers/order/create-order.ts
@@ -1,11 +1,15 @@
+import { HashID } from "@/domain";
 import { Post, RequiredParams } from "@/presentation/decorators";
 import { HttpRequest } from "@/presentation/protocols";
 
 
 @Post('/order')
 export class CreateOrderController {
+    constructor( private readonly encoder: HashID ){}
     @RequiredParams(['customer', 'address'])
     async handle(request: HttpRequest){
+        const { customer } = request.body
+        this.encoder.decode(customer)
         return request
     }
 }
