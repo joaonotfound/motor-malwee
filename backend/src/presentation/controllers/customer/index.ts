f63ed9552f1c74479d4a64929daa6e1677cc55c6
joaonotfound <joaonotfound@protonmail.com>
2022-11-11 13:11:15 -0300

    feat: should call decoder

--- backend/src/presentation/controllers/customer/index.ts
+++ backend/src/presentation/controllers/customer/index.ts
@@ -1,1 +1,2 @@
 export * from './load-customer'
+export * from './load-orders'
--- backend/src/presentation/controllers/customer/load-orders.ts
+++ backend/src/presentation/controllers/customer/load-orders.ts
@@ -1,11 +1,15 @@
+import { HashID } from "@/domain";
 import { Get, RequiredParams } from "@/presentation/decorators";
 import { HttpRequest } from "@/presentation/protocols";
 
 
 @Get('/customer/orders')
 export class LoadOrdersController {
+    constructor( private encoder: HashID ){}
     @RequiredParams(['customer'], { on: "params" })
     async handle(request: HttpRequest){
-        return request
+        const { customer } = request.params
+        const privateId = this.encoder.decode(customer)
+        return privateId
     }
 }
