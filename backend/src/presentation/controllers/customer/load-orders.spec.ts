151f3f9986021246a785ebbaf9f7f5173bec40aa
joaonotfound <joaonotfound@protonmail.com>
2022-11-11 13:11:19 -0300

    test: should call decoder

--- backend/src/presentation/controllers/customer/load-orders.spec.ts
+++ backend/src/presentation/controllers/customer/load-orders.spec.ts
@@ -1,10 +1,12 @@
-import { missingParam } from "@/presentation/helpers"
+
+import { makeHashIDStub, missingParam } from "@/presentation/helpers"
 import { HttpRequest } from "@/presentation/protocols"
 import { LoadOrdersController } from "./load-orders"
 
 const makeSut = () => {
-    const sut = new LoadOrdersController()
-    return { sut }
+    const idHasher = makeHashIDStub()
+    const sut = new LoadOrdersController(idHasher)
+    return { sut, idHasher }
 }
 
 describe('LoadOrdersController', () => {
@@ -16,5 +18,17 @@ describe('LoadOrdersController', () => {
         }
         const response = await sut.handle(request)
         expect(response).toEqual(missingParam('customer'))
+    })
+    it('should call decoder', async () => {
+        const { sut, idHasher } = makeSut()
+        const decodeSpy = jest.spyOn(idHasher, 'decode')
+        const request: HttpRequest = {
+            body: {},
+            params: {
+                customer: "valid-customer"
+            }
+        }
+        await sut.handle(request)
+        expect(decodeSpy).toHaveBeenCalledWith('valid-customer')
     })
 })
