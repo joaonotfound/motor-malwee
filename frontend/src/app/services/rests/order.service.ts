2549ff54689beb71a985a4564c8014b04fe240c8
joaonotfound <joaonotfound@protonmail.com>
2022-11-15 11:27:53 -0300

    chore: create order service

---
+++ frontend/src/app/services/rests/order.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Order } from 'src/app/models';
+import { createAxios } from 'src/helpers/create-axios';
+import { AuthenticationService } from '../authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrdersService {
+
+    constructor(private readonly auth: AuthenticationService) { }
+
+    public async create(order: Order): Promise<boolean> {
+        const axios = createAxios(this.auth.getToken())
+        const response = await axios.post('/order', order)
+        if (response.data.created) {
+            return true
+        }
+        return false
+    }
+}
