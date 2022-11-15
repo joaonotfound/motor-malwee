12c7fea91da0d43d401103adafa9056dec902cd5
joaonotfound <joaonotfound@protonmail.com>
2022-11-15 16:22:40 -0300

    feat: handle orders items on edit-orders-controller

--- backend/src/presentation/controllers/order/edit-order.ts
+++ backend/src/presentation/controllers/order/edit-order.ts
@@ -1,19 +1,39 @@
-import { HashID, orderEntity, Repository } from "@/domain";
+import { HashID, OrderEntity, orderEntity, Repository } from "@/domain";
+import { OrderItem } from "@/infra";
 import { Put, RequiredParams } from "@/presentation/decorators";
 import { invalidParam } from "@/presentation/helpers";
 import { HttpRequest } from "@/presentation/protocols";
 
+const handleOrdersItem =  async (ordersItem: Partial<OrderItem>, repository: Repository, orderID: number) => {
+    const isValidOrdersItem = () => {
+        const keys1 = Object.keys({ ...ordersItem, id: '' }).sort()
+        const keys2 = Object.keys(orderEntity).sort()
+        return JSON.stringify(keys1) == JSON.stringify(keys2)        
+    }
+    if(!isValidOrdersItem()){
+        return
+    }
+    if(!ordersItem.id){
+        return await repository.collection(orderEntity).save({ ...ordersItem, id: orderID } as OrderEntity)
+    }
+    return await repository.collection(orderEntity).update(ordersItem as OrderEntity)
+
+}
 @Put('/orders')
 export class EditOrderController {
     constructor( private readonly repository: Repository, private readonly encoder: HashID ){}
     @RequiredParams(['id'])
     async handle(request: HttpRequest){
-        const { id } = request.body
+        const { id, ordersItems } = request.body
         const publicID = this.encoder.decode(id)    
         
         const matchOrder = await this.repository.collection(orderEntity).findOne({ id: publicID })
         if(!matchOrder){
             return invalidParam('id')
+        }
+
+        for(const ordersItem of ordersItems){
+            await handleOrdersItem(ordersItem, this.repository, publicID)
         }
 
         return request
