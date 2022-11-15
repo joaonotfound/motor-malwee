fd4c07a42ae77a1197c688d3641bafd99fc7ec4c
joaonotfound <joaonotfound@protonmail.com>
2022-11-15 17:34:11 -0300

    feat: create table component

--- frontend/src/app/pages/order-item/order-item.component.ts
+++ frontend/src/app/pages/order-item/order-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Column } from 'src/app/components/table/table.component';
+import { OrdersItemsService } from 'src/app/services/rests/orders-items.service';
 
 @Component({
   selector: 'app-order-item',
@@ -8,7 +10,28 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class OrdersItemComponent implements OnInit {
 
-  constructor( private readonly route: ActivatedRoute ) { }
+  items: any = []
+  params: any = {}
+  table_columns: Column[] = [
+    { columnName: "Produto", propertyName: 'productDescription'},
+    { columnName: 'Quantidade', propertyName: "quantity" },
+    { columnName: 'UnitPrice', propertyName: "unitPrice" },
+    { columnName: 'TotalPrice', propertyName: "totalPrice" }
+  ]
+
+  constructor( 
+    private readonly route: ActivatedRoute,
+    private readonly itemsService: OrdersItemsService
+  ) {
+    this.route.queryParams.subscribe(params => {
+      this.params = params
+      this.loadItems()
+    })
+  }
+
+  async loadItems(){
+    this.items = await this.itemsService.load(this.params.id)
+  }
 
   ngOnInit(): void {
   }
---
+++ frontend/src/app/services/rests/orders-items.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Order } from 'src/app/models';
+import { createAxios } from 'src/helpers/create-axios';
+import { AuthenticationService } from '../authentication.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrdersItemsService {
+
+    constructor(private readonly auth: AuthenticationService) { }
+    
+    public async load(orderID: string): Promise<Order[]>{
+        const axios = createAxios(this.auth.getToken())
+        const response = await axios.get('/orders/items', { params: { id: orderID }})
+        return response.data.orders
+    }
+}
