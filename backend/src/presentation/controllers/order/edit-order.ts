0114f850d269d8fdeab0d56e7be7db3a77a8ad1b
joaonotfound <joaonotfound@protonmail.com>
2022-11-15 12:48:34 -0300

    feat: should return 400 if invalid id is providen

--- backend/src/presentation/controllers/order/edit-order.ts
+++ backend/src/presentation/controllers/order/edit-order.ts
@@ -1,10 +1,21 @@
+import { HashID, orderEntity, Repository } from "@/domain";
 import { Put, RequiredParams } from "@/presentation/decorators";
+import { invalidParam } from "@/presentation/helpers";
 import { HttpRequest } from "@/presentation/protocols";
 
 @Put('/orders')
 export class EditOrderController {
+    constructor( private readonly repository: Repository, private readonly encoder: HashID ){}
     @RequiredParams(['id'])
     async handle(request: HttpRequest){
+        const { id } = request.body
+        const publicID = this.encoder.decode(id)    
+        
+        const matchOrder = await this.repository.collection(orderEntity).findOne({ id: publicID })
+        if(!matchOrder){
+            return invalidParam('id')
+        }
+
         return request
     }
 }
