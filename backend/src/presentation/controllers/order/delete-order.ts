e4f4a586ba535c80df0c448858f6791b94d98526
joaonotfound <joaonotfound@protonmail.com>
2022-11-16 13:24:48 -0300

    feat: should call decoder

--- backend/src/presentation/controllers/order/delete-order.ts
+++ backend/src/presentation/controllers/order/delete-order.ts
@@ -1,10 +1,14 @@
+import { HashID } from "@/domain";
 import { RequiredParams } from "@/presentation/decorators";
 import { HttpRequest } from "@/presentation/protocols";
 
 
 export class DeleteOrderController {
+    constructor( private readonly encoder: HashID ){}
     @RequiredParams(['id'])
     async handle(request: HttpRequest){
-        return request
+        const { id } = request.body
+        const privateID = this.encoder.decode(id)
+        return privateID
     }
 }
