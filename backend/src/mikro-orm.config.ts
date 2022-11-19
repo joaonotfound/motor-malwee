0b133846d9cfa38b4d478bc869b79e31f8585b34
joaonotfound <joaonotfound@protonmail.com>
2022-11-18 23:04:33 -0300

    chore: create docker-compose file

--- backend/src/mikro-orm.config.ts
+++ backend/src/mikro-orm.config.ts
@@ -5,13 +5,17 @@ import * as dotenv from 'dotenv';
 
 dotenv.config()
 
+const getDatabasePort = (): number => process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306
+
 const config: Configuration<IDatabaseDriver<Connection>> | Options<IDatabaseDriver<Connection>> = {
     entities: [ User, Group, SubGroup, Collection, Product, Customer, Address, Order, OrderItem ],
     type: 'mysql',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'root',
-    dbName: 'sales-engine',
-    debug: false,
+    dbName: process.env.DB_NAME || 'sales-engine',
+    host: process.env.DB_HOST || 'localhost',
+    port: getDatabasePort(),
+    debug: false,    
     namingStrategy: EntityCaseNamingStrategy,
     allowGlobalContext: true,
     logger: (message: string) => console.log(`[ORM] - ${message}`)
